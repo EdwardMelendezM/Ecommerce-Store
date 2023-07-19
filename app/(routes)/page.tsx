947cf52da09d09de108370976e9922285b783295
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-19 16:56:06 -0500

    adding billboard slider image

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,24 @@
-const Home = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+import { Billboard as BillboardType } from "@/types";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  // @ts-ignore
+  const billboard: BillboardType[]  = await getBillboard("e107eb78-330c-4309-8faf-8897a108801f")
+
+
   return ( 
-    <div>
-      Hello store
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard[0]}  />
+      </div>
+    </Container>
    );
 }
  
-export default Home;
+export default HomePage;
